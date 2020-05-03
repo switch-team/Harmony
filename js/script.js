acf6f7277c682472f7ec96d1bed904cf53da2c46
Maher Akkari <maherakkari1@gmail.com>
2020-05-03 23:52:52 +0300

    primery commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#my-carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause'))
+        {
+            $("#my-carousel").carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+
+        }
+        else
+        {
+            $("#my-carousel").carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    
+});
