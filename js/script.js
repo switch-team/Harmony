65e27cefeea0547d28172f7006eed89d82874043
MayyasNakhli <38408148+MayyasNakhli@users.noreply.github.com>
2020-05-07 20:30:19 +0300

    Harmony Family

--- js/script.js
+++ js/script.js
@@ -15,5 +15,39 @@ $(document).ready(function(){
             $('#carouselButton').children('span').addClass('fa-pause');
         }
     });
+
+    //Family Scroliing
+    $('#scroll-down').click(function(){
+        $('#Family-Container').scrollTo('+=258px');
+    });
     
+    $('#scroll-up').click(function(){
+        $('#Family-Container').scrollTo('-=258px');
+    });
+    //End Family Scroliing
+    
+    
+      
+     //Family Randomizing 
+    (function($) {
+    
+    $.fn.randomize = function(tree, childElem) {
+        return this.each(function() {
+        var $this = $(this);
+        if (tree) $this = $(this).find(tree);
+        var unsortedElems = $this.children(childElem);
+        var elems = unsortedElems.clone();
+    
+        elems.sort(function() {
+            return (Math.round(Math.random()) - 0.5);
+        });
+    
+        for (var i = 0; i < elems.length; i++)
+            unsortedElems.eq(i).replaceWith(elems[i]);
+        });
+    };
+    
+    })(jQuery);
+    $("#Family-Container").randomize();
+    //End Family Randomizing 
 });
